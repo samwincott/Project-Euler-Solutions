2c7bb6873c4b787e8148847ae0c1320ea5799749
samwincott <wincott97@gmail.com>
2019-01-02 16:13:38 +0000

    Ninth problem complete in Python

---
+++ problem9.py
@@ -0,0 +1,16 @@
+"""
+This solves problem 9 of the project euler problems.
+Found at https://projecteuler.net/problem=9
+"""
+
+def compute():
+    """Calculates a*b*c where a^2+b^2=c^2 and a+b+c=1000"""
+    for a in range(1, 1000):
+        for b in range(a, 1000):
+            c = 1000 - a - b
+            if c > 0:
+                if c * c == a*a + b*b:
+                    return a*b*c
+
+answer = compute()
+print("Answer:", answer)
